036763c69e16800d53662652fe66fb17d99746b3
Andreas <tb.tapion@gmail.com>
2023-07-03 21:04:36 +0200

    data loading from local storage
    -Need to commit for testing on static pages

--- src/pages/load.ts
+++ src/pages/load.ts
@@ -3,9 +3,6 @@ import keys from "../util/keys";
 
 function loadFromLocalStorage() {
     const sheets : string = localStorage.getItem(keys.localStorageKey) ?? '{ "error":"SheetNotFound"} ';
-
-    console.log(sheets);
-    console.log(JSON.parse(sheets))
     return JSON.parse(sheets);
 }
 
